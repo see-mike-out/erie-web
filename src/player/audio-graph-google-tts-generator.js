d22d012800b040b65a3e4b498e25ea30ebbb0450
Hyeok Kim <hyeokkim@dhcp-10-105-57-159.wireless.northwestern.private>
2024-02-27 18:36:33 -0600

    patch

--- src/player/audio-graph-google-tts-generator.js
+++ src/player/audio-graph-google-tts-generator.js
@@ -1,12 +1,15 @@
 import * as tts from "@google-cloud/text-to-speech";
 import { bcp47language } from "./audio-graph-player-proto";
 
-export async function GoogleCloudTTSGenerator(sound, config, resolve, options) {
+const SSMLGENDERS = [`NEUTRAL`, `FEMALE`, `MALE`];
+
+export async function GoogleCloudTTSGenerator(sound, config) {
   if (typeof window === 'undefined') {
     // node
     let text = sound.speech;
-    let languageCode = bcp47language.includes(sound.language) ? sound.language : 'en-US';
-    let ssmlGender = config.ssmlGender || 'NEUTRAL';
+    let lang = sound.language || config.language;
+    let languageCode = bcp47language.includes(lang) ? lang : 'en-US';
+    let ssmlGender = SSMLGENDERS.includes(config.ssmlGender) ? config.ssmlGender : 'NEUTRAL';
     let pitch = sound.pitch, speakingRate = sound.speechRate || config.speechRate || 1;
     const request = {
       input: { text: text },
@@ -17,9 +20,8 @@ export async function GoogleCloudTTSGenerator(sound, config, resolve, options) {
     // Performs the text-to-speech request
     const [response] = await client.synthesizeSpeech(request);
     return response.audioContent;
-    // resolve();
   } else {
     console.warn("This function can only be run on node server environment");
-    resolve();
+    return null;
   }
 }
